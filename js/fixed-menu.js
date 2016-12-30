6e7062e980d0ea7c6619162e37164a969f3c11a5
VladHTMLCSS <vlad.kandyba.00@mail.ru>
2016-12-30 21:20:00 +0200

    Change brand

--- js/fixed-menu.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > 50) {
-			$('.header').addClass('fixed');
-		} else	 {
-			$('.header').removeClass('fixed');
-		}
-	});
-});
